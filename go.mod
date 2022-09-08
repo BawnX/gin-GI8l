72a7f27763633456cf68192b636f7b3ef249ff81
Antonio Morales <antonio.morales@anschile.cl>
2022-09-08 13:37:57 -0300

    feat: added initial config

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module gin
+module service-users
 
-go 1.14
+go 1.15
 
-require github.com/gin-gonic/gin v1.7.1 // indirect
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/vektah/gqlparser/v2 v2.1.0
+)
--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+	"service-users/graph"
+	"service-users/graph/generated"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
-var Router * gin.Engine
+
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello world!",
-		})
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome!")
 	})
-	r.Run()
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	e.POST("/query", func(c echo.Context) error {
+		srv.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	e.GET("/playground", func(c echo.Context) error {
+		playground.Handler("GraphQL", "/query").ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	e.Logger.Fatal(e.Start(":4001"))
 }
